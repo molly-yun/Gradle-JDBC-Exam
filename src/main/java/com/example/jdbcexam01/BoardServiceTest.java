41a7c8fed0644efad80bf5cb0b9fc44cdb1ad801
molly-yun <mollyyun91@gmail.com>
2022-07-27 22:39:59 +0900

    220723

--- src/main/java/com/example/jdbcexam01/BoardServiceTest.java
+++ src/main/java/com/example/jdbcexam01/BoardServiceTest.java
@@ -8,20 +8,20 @@ public class BoardServiceTest {
             BoardService boardService = new BoardService();
 
             // 목록 조회
-            List<Board> boards = boardService.getBoards(0, 30);
-
-            for (Board board : boards) {
-                System.out.println(board);
-            }
+//            List<Board> boards = boardService.getBoards(0, 30);
+//
+//            for (Board board : boards) {
+//                System.out.println(board);
+//            }
 
             // 글쓰기
-//            Board board = new Board("둘리", "여름이다", "빙하기 그립다" );
+//            Board board = new Board("콩콩", "내친구 콩순이", "콩순이 좋아" );
 //            int updateCount = boardService.addBoard(board);
 //            System.out.println(updateCount);
 
             // 해당 id 글 조회
-//            Board board = boardService.getBoard(5L);
-//            System.out.println(board);
+            Board board = boardService.getBoard(6L);
+            System.out.println(board);
 
         } catch(Exception ex) {
             ex.printStackTrace();
